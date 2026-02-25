1f5414a217c0948c05c43c88f4acd63bfb78a4d3
Victor Paredes <vparedes@ejemplo.com>
2026-02-25 13:18:39 -0300

    Conectando Dashboard a la nueva URL maestra

--- src/services/api.js
+++ src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbx-sYcf6lFbaeXplcBXMaTrKUkX5_NLXFgSyL2yMw-JpNQuhXfMLB0zcSpsNJy5XDwX3w/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbwt-D6SS1EoNnEuliY6Qae-fDJP3-2Yz5w0PH6QjDBjw8UuMDxNK7YFR0TJCjE8_sp0Ww/exec";
 
 export const sendToSheet = async (data) => {
     if (!API_URL) {
